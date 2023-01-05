dc8ae6c11e0f6f715a6b338fde8886fbfdda71a5
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-05 11:51:32 +0100

    finishing mini project

--- 11_MiniProject/dist/index.js
+++ 11_MiniProject/dist/index.js
@@ -1,6 +1,50 @@
-'use strict';
+"use strict";
 function printMsg(msg) {
-	console.log(msg);
-	console.log(msg);
+    console.log(msg);
+    console.log(msg);
 }
 printMsg('Hello Typescript');
+var btn = document.getElementById('btn'); //exclamation mark tells TS that value won't be null
+var input = document.getElementById('todoinput'); // as ... - type assertion  
+var form = document.querySelector('form');
+var list = document.getElementById('todolist');
+var todos = readLocalStorage();
+todos.forEach(createTodo);
+// btn?.addEventListener('click', function () { //? optional chaining do if btn not null
+//     alert(input.value)
+//     input.value = ''
+// })
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(evt) {
+    evt.preventDefault();
+    var newTodo = {
+        text: input.value,
+        completed: false
+    };
+    todos.push(newTodo);
+    saveTodos();
+    createTodo(newTodo);
+    input.value = '';
+}
+function createTodo(todo) {
+    var newLi = document.createElement('li');
+    var checkInput = document.createElement('input');
+    checkInput.type = 'checkbox';
+    checkInput.checked = todo.completed;
+    checkInput.addEventListener('change', function () {
+        todo.completed = checkInput.checked;
+        saveTodos();
+    });
+    newLi.append(todo.text);
+    newLi.append(checkInput);
+    list.append(newLi);
+}
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+function readLocalStorage() {
+    var localTodos = localStorage.getItem('todos');
+    if (localTodos === null)
+        return [];
+    return JSON.parse(localTodos);
+}
--- 11_MiniProject/src/index.ts
+++ 11_MiniProject/src/index.ts
@@ -4,3 +4,63 @@ function printMsg(msg: string): void {
 }
 
 printMsg('Hello Typescript')
+
+interface Todo {
+    text: string;
+    completed: boolean;
+}
+
+
+const btn = document.getElementById('btn')!; //exclamation mark tells TS that value won't be null
+const input = document.getElementById('todoinput')! as HTMLInputElement; // as ... - type assertion  
+const form = document.querySelector('form')!;
+const list = document.getElementById('todolist') as HTMLUListElement;
+
+const todos: Todo[] = readLocalStorage();
+todos.forEach(createTodo);
+
+// btn?.addEventListener('click', function () { //? optional chaining do if btn not null
+//     alert(input.value)
+//     input.value = ''
+// })
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt: SubmitEvent) {
+    evt.preventDefault();
+
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false
+    }
+    todos.push(newTodo);
+    saveTodos();
+    createTodo(newTodo);
+    input.value = ''
+}
+
+function createTodo(todo: Todo) {
+    const newLi = document.createElement('li');
+    const checkInput = document.createElement('input');
+    checkInput.type = 'checkbox'
+    checkInput.checked = todo.completed
+
+    checkInput.addEventListener('change', function () {
+        todo.completed = checkInput.checked;
+        saveTodos()
+    })
+
+    newLi.append(todo.text);
+    newLi.append(checkInput);
+    list.append(newLi);
+}
+
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+function readLocalStorage(): Todo[] {
+    const localTodos = localStorage.getItem('todos')
+    if (localTodos === null) return [];
+    return JSON.parse(localTodos);
+}
