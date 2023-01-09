bc7b74eabfbc320c92ba7fcba1cac1191c665087
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-09 21:58:21 +0100

    adding export and import and switch to module

--- 17_working_with_modules/src/index.ts
+++ 17_working_with_modules/src/index.ts
@@ -1,4 +1,5 @@
 //in TS functions from utils are visible in index
 //in JS you have to import files in correct order using <script> tag
+import { add } from './utils';
 add(5, 5);
 sample([1, 2, 3, 4, 5, 6]);
