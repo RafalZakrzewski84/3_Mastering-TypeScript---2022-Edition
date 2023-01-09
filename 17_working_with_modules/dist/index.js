da49fd387cc344b3138c6dbcba33c1dec8d4257a
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-09 22:03:36 +0100

    compile ts file to es2016 and module commonjs

--- 17_working_with_modules/dist/index.js
+++ 17_working_with_modules/dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 //in TS functions from utils are visible in index
 //in JS you have to import files in correct order using <script> tag
-add(5, 5);
-sample([1, 2, 3, 4, 5, 6]);
+const utils_1 = require("./utils");
+(0, utils_1.add)(5, 5);
+(0, utils_1.sample)([1, 2, 3, 4, 5, 6]);
