aed1b35071bec63a2dbc0f26546a2cac1f731e8d
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-09 21:54:16 +0100

    initialize files for module lesson

---
+++ 17_working_with_modules/src/index.ts
@@ -0,0 +1,4 @@
+//in TS functions from utils are visible in index
+//in JS you have to import files in correct order using <script> tag
+add(5, 5);
+sample([1, 2, 3, 4, 5, 6]);
---
+++ 17_working_with_modules/src/utils.ts
@@ -0,0 +1,8 @@
+function add(x: number, y: number): number {
+	return x + y;
+}
+
+function sample<T>(arr: T[]): T {
+	const idx = Math.floor(Math.random() * arr.length);
+	return arr[idx];
+}
