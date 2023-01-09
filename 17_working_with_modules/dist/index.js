09478dae84918bc511a66aebacd2735091c7a74a
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-09 22:14:19 +0100

    changing compile module to es6

--- 17_working_with_modules/dist/index.js
+++ 17_working_with_modules/dist/index.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 //in TS functions from utils are visible in index
 //in JS you have to import files in correct order using <script> tag
-const utils_1 = require("./utils");
-(0, utils_1.add)(5, 5);
-(0, utils_1.sample)([1, 2, 3, 4, 5, 6]);
+import { add, sample } from './utils';
+add(5, 5);
+sample([1, 2, 3, 4, 5, 6]);
