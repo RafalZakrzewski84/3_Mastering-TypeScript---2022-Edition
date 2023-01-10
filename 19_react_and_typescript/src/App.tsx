8e58ea60426d13a728a5a6d08f4dafc3203a1092
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-10 15:30:16 +0100

    component with type react function

--- 19_react_and_typescript/src/App.tsx
+++ 19_react_and_typescript/src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 
 import Greeter from './components/Greeter';
+import GreeterArrow from './components/GreeterArrow';
 
 function App() {
 	return (
 		<div className="App">
-			<Greeter />
-			<Greeter />
+			<GreeterArrow />
 			<Greeter />
 		</div>
 	);
---
+++ 19_react_and_typescript/src/components/GreeterArrow.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GreeterArrow: React.FC = () => {
+	return <h1>Hello Arrow</h1>;
+};
+
+export default GreeterArrow;
