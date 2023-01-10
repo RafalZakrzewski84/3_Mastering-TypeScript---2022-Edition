32b98a39eed54838d81796615e4df62537fd5f21
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-10 15:23:26 +0100

    basic component return type JSX.element

--- 19_react_and_typescript/src/App.tsx
+++ 19_react_and_typescript/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Greeter from './components/Greeter';
+
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World</h1>
+			<Greeter />
+			<Greeter />
+			<Greeter />
 		</div>
 	);
 }
