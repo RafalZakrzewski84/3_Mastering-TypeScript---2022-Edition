8df90d26226260444fede322379fb2da8840ae1e
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-10 15:53:42 +0100

    adding shopping list component and types for props

--- 19_react_and_typescript/src/App.tsx
+++ 19_react_and_typescript/src/App.tsx
@@ -3,12 +3,18 @@ import './App.css';
 
 import Greeter from './components/Greeter';
 import GreeterArrow from './components/GreeterArrow';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
+	const items = [
+		{ id: 1, product: 'Lemon', qty: 2 },
+		{ id: 2, product: 'Apple', qty: 10 },
+	];
 	return (
 		<div className="App">
 			<GreeterArrow />
 			<Greeter person="Rafal" />
+			<ShoppingList items={items} />
 		</div>
 	);
 }
