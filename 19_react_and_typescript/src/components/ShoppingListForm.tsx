5281cbd70f6e98b0de827208096d53937281b2d9
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-10 21:54:47 +0100

    adding useRef and type for it

--- 19_react_and_typescript/src/components/ShoppingListForm.tsx
+++ 19_react_and_typescript/src/components/ShoppingListForm.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function ShoppingListForm(): JSX.Element {
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	function handleSubmit(evt: React.FormEvent) {
+		evt.preventDefault();
+		console.log(inputRef);
+		console.log(inputRef.current?.value);
+	}
+
 	return (
-		<form>
-			<input type="text" placeholder="Product name" />
+		<form onSubmit={handleSubmit}>
+			<input type="text" placeholder="Product name" ref={inputRef} />
 			<button type="submit">Add item</button>
 		</form>
 	);
